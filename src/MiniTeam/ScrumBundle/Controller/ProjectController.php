5d2210a8848d58d8b5333e420306e7208e56d968
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2013-01-21 21:53:50 +0100

    Redirect user to its first project when accessing "/"

--- src/MiniTeam/ScrumBundle/Controller/ProjectController.php
+++ src/MiniTeam/ScrumBundle/Controller/ProjectController.php
@@ -15,8 +15,15 @@ class ProjectController extends Controller
      * @Extra\Route("/{name}")
      * @Extra\Template()
      */
-    public function showAction(Project $project)
+    public function showAction(Project $project = null)
     {
+        if (null == $project) {
+            $project = $this->getUser()->getSelectedProject();
+            $url     = $this->generateUrl('miniteam_scrum_project_show', array('name' => $project->getName()));
+
+            return $this->redirect($url);
+        }
+
         return array('project' => $project);
     }
     
