d5fa09d9e08e6ab2cf7c8d04eb93b409b4044981
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2013-01-26 23:57:39 +0100

    Implement adding user stories

--- features/story.feature
+++ features/story.feature
@@ -9,11 +9,11 @@ Feature: User story features
     As a product owner
     I add a user story to my project
 
-    Given I am on "/mini-scrum/us/new"
+    Given I am authenticated as "mini" with "test"
+    And I am on "/mini-scrum/us/new"
     When I fill in the following:
-      | title   | "As a product owner I add a story in the backlog" |
-      | details | "I can add some details"                          |
-      | points  | 3                                                 |
+      | story_title   | "As a product owner I add a story in the backlog" |
+      | story_details | "I can add some details"                          |
+      | story_points  | 3                                                 |
     And I press "Save"
-    Then I should be redirected
-    And I should see "As a product owner I add a story in the backlog" in the "p.lead" element
+    Then I should see "As a product owner I add a story in the backlog" in the "p.lead" element
--- src/MiniTeam/ScrumBundle/Controller/StoryController.php
+++ src/MiniTeam/ScrumBundle/Controller/StoryController.php
@@ -2,12 +2,18 @@
 
 namespace MiniTeam\ScrumBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
+use MiniTeam\ScrumBundle\Entity\Project;
 use MiniTeam\ScrumBundle\Entity\UserStory;
+use MiniTeam\ScrumBundle\Form\UserStoryType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as Extra;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Form;
 
 /**
  * @author Benjamin Grandfond <benjamin.grandfond@gmail.com>
+ * @author Edouard de Labareyre <edouard@melix.net>
  *
  * @Extra\Route("/{projectName}")
  */
@@ -23,7 +29,7 @@ class StoryController extends Controller
     }
 
     /**
-     * @Extra\Route("/us/{id}", requirements={"id" = "\d+"})
+     * @Extra\Route("/us/{id}", name="story_show", requirements={"id" = "\d+"})
      * @Extra\Template()
      */
     public function showAction(UserStory $story)
@@ -33,10 +39,55 @@ class StoryController extends Controller
 
     /**
      * @Extra\Route("/us/new", name="story_new")
+     * @Extra\ParamConverter("project", options={"mapping": {"projectName": "name"}})
      * @Extra\Template()
      */
-    public function newAction($projectName)
+    public function newAction(Project $project, Request $request)
     {
-        return array('projectName' => $projectName);
+        $story = new UserStory();
+        $story->setProject($project);
+
+        $form = $this->createForm(new UserStoryType(), $story);
+
+        if ($request->isMethod('post')) {
+            $response = $this->processForm($request, $form);
+
+            if ($response instanceof RedirectResponse) {
+                return $response;
+            }
+        }
+
+        return array('project' => $project, 'form' => $form->createView());
+    }
+
+    /**
+     * Process the form of a UserStory
+     *
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\Form\Form              $form
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    protected function processForm(Request $request, Form $form)
+    {
+        $form->bind($request);
+
+        if ($form->isValid()) {
+            $story = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($story);
+            $em->flush();
+
+            return $this->redirect(
+                $this->generateUrl(
+                    'story_show',
+                    array(
+                        'projectName' => $story->getProject()->getName(),
+                        'id' => $story->getId()
+                    )
+                )
+            );
+        }
     }
 }
